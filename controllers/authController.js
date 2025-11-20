8ae5ab2bfcb5c1edd603ef1115008e31a6536707
Varun Pandey <varunp5324@gmail.com>
2025-11-21 00:38:19 +0530

    Fixing Logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,6 +83,7 @@ export const Login = async (req,res)=>{
 }
 
 export const Logout = async (req, res) => {
+    req.user=null;
     return res.status(201).cookie("token", "", { expiresIn: new Date(Date.now() - 3600000) }).json({
         message: `Logged out Successfully`,
         success: true
